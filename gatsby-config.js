c8743a447c87436e5c2839e0a460604b5fbe2e91
sebastiano <36180003+seibas@users.noreply.github.com>
2021-01-16 15:17:26 +0900

    change sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Beba Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Vitturi Sebastiano`,
+      summary: `who lives and works in Tokyo and lernin web developement.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `testing this blog.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `Beba`,
     },
   },
   plugins: [
